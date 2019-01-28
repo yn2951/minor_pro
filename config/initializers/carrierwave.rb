2ebedfe82a69ee506b73678638e7f93cd923e9b7
yn2951 <keichichiba@gmail.com>
2019-01-28 20:32:05 +0900

    #

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['S3_access_key'],
+    aws_secret_access_key: ENV['S3_access_secret'],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'minor-pro'
+  config.cache_storage = :fog
+end
