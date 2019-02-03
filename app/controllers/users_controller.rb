98c62d79979232008b23caebe74089e0123d840e
yn2951 <keichichiba@gmail.com>
2019-02-03 18:19:58 +0900

    #

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
     if User.find_by(email: @user.email).nil?
       if @user.save && profile.save
-        UserResisterationMailer.welcome(@user).deliver_now
+        # UserResisterationMailer.welcome(@user).deliver_now
         redirect_to root_path, success: 'メールを送信しました'
       else
         redirect_to new_user_path, danger: '登録に失敗しました'
