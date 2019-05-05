e802d39b67b2807c0e1d25b1451654bf0dfd75de
yn2951 <keichichiba@gmail.com>
2019-05-05 20:25:35 +0900

    detail update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.create(comment_params)
-    new_comment_count = set_counter.comment_count.next
-    set_counter.update(comment_count: new_comment_count)
-    @topic = @comment.topic
-    flash[:notice] = "コメントを投稿しました"
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      new_comment_count = set_counter.comment_count.next
+      set_counter.update(comment_count: new_comment_count)
+      @topic = @comment.topic
+      flash[:notice] = "コメントを投稿しました。"
+    else
+      flash[:alert] = "コメントの投稿に失敗しました。"
+    end
   end
 
   def comment_params
